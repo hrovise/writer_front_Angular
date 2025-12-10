4956a4a109cb55a6b0556fcb3d0c16328cd00e3a
hrovi <hrovise@gmail.com>
2025-12-10 14:09:44 +0100

    add http to test

--- src/core/authservice/authservice.spec.ts
+++ src/core/authservice/authservice.spec.ts
@@ -2,13 +2,16 @@ import { TestBed } from '@angular/core/testing';
 
 import { Authservice } from './authservice';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('Authservice', () => {
   let service: Authservice;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideZonelessChangeDetection()]
+      providers: [provideZonelessChangeDetection(),
+       provideHttpClient(),        
+        provideHttpClient() ]
     });
     service = TestBed.inject(Authservice);
   });
--- src/core/file-service/file-service.spec.ts
+++ src/core/file-service/file-service.spec.ts
@@ -2,13 +2,16 @@ import { TestBed } from '@angular/core/testing';
 
 import { FileService } from './file-service';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('FileService', () => {
   let service: FileService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideZonelessChangeDetection()]
+      providers: [provideZonelessChangeDetection(),
+       provideHttpClient(),        
+        provideHttpClient()  ]
     });
     service = TestBed.inject(FileService);
   });
