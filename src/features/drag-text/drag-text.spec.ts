97a646b7cd99ebe1df6af9d228a1e6d3dc3b776f
hrovi <hrovise@gmail.com>
2025-12-10 14:36:39 +0100

    add mock

--- src/features/drag-text/drag-text.spec.ts
+++ src/features/drag-text/drag-text.spec.ts
@@ -4,18 +4,30 @@ import { DragTextComponent } from './drag-text';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { FileService } from '../../core/file-service/file-service';
+import { of } from 'rxjs';
 
 describe('DragTextComponent', () => {
   let component: DragTextComponent;
   let fixture: ComponentFixture<DragTextComponent>;
-
+ let mockFileService: any;
+ 
   beforeEach(async () => {
+      mockFileService = jasmine.createSpyObj('FileService', ['getChunk', 'getAllTexts']);
+
+   
+    mockFileService.getChunk.and.returnValue(of({ content: 'Fake Text' })); 
+    mockFileService.getAllTexts.and.returnValue(of([]));
+
     await TestBed.configureTestingModule({
       imports: [DragTextComponent],
-       providers: [provideZonelessChangeDetection(),
-        provideHttpClient(),        
-        provideHttpClient(),
-      provideRouter([]) ]
+      providers: [
+        provideZonelessChangeDetection(),
+        
+        { provide: FileService, useValue: mockFileService },
+        
+    
+      ]
     })
     .compileComponents();
 
